89cda9a1797eaf954746f7b8325737ba54783956
Gaku Ueda <gaku@gaku.net>
2014-06-22 01:47:29 -0700

    Read two pins and dump them to the serial console.

--- sketch_jun18a.ino
+++ sketch_jun18a.ino
@@ -1,17 +1,33 @@
 // Using Arduino MEGA's pin 3 (PORT E's 4th pin).
 // 4th bit from MSB, not LSB.
 //
+int a[16];
+
 void setup() {
+  // Pin 3 for clock output
   DDRE = B00010000;
+  
+  pinMode(A0, INPUT);
+
+  Serial.begin(57600);
+  Serial.println("HELLO");
 }
 
 void loop() {
+  int i;
   while (1) {    
     // roughly 2usec clock
     PORTE = B00000000;
     delayMicroseconds(1);
     PORTE = B00010000;
-    delayMicroseconds(1);
+    //delayMicroseconds(10000);
+    delay(100);
+    a[0] = digitalRead(A0);
+    a[1] = digitalRead(A1);
+    for (i = 0; i < 2; i++) {
+      Serial.print(a[i]);
+    }
+    Serial.println();    
   }
 }
 
